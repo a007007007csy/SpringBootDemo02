f9b66b3804b6a52aa3497b55fbe8326834ce75f5
amyny <a007007007csy86@gmail.com>
2020-03-24 21:31:39 +0800

    @Async+@scheduled

--- src/main/java/com/springboot/demo11task/Demo11TaskApplication.java
+++ src/main/java/com/springboot/demo11task/Demo11TaskApplication.java
@@ -3,7 +3,9 @@ package com.springboot.demo11task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling // 开启定时任务注解
 @EnableAsync // 开启异步注解
 @SpringBootApplication
 public class Demo11TaskApplication {
