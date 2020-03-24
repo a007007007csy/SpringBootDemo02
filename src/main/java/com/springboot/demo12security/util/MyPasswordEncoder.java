6fbce2cd0aec432c8fd26a69b83bdfaa3a2a381f
amyny <a007007007csy86@gmail.com>
2020-03-24 23:52:59 +0800

    init-SpringSecurity

---
+++ src/main/java/com/springboot/demo12security/util/MyPasswordEncoder.java
@@ -0,0 +1,20 @@
+package com.springboot.demo12security.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyPasswordEncoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence charSequence) {
+        return charSequence.toString();
+    }
+
+    @Override
+    public boolean matches(CharSequence charSequence, String s) {
+        return s.equals(charSequence.toString());
+    }
+}
+
---
+++ src/test/java/com/springboot/demo12security/Demo12SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo12security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo12SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
